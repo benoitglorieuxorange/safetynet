bbae79d6b6c7391e0f288cb28a252095c5574a43
Benoit GLORIEUX <benoit.glorieux@orange.com>
2026-03-25 09:03:39 +0100

    add entities person and FireStation

---
+++ src/main/java/com/globe/safetynet/entities/Person.java
@@ -0,0 +1,81 @@
+package com.globe.safetynet.entities;
+
+public class Person {
+
+    //"firstName":"John", "lastName":"Boyd", "address":"1509 Culver St", "city":"Culver", "zip":"97451", "phone":"841-874-6512", "email":"jaboyd@email.com"
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private String zip;
+    private String phone;
+    private String email;
+
+    public Person(){}
+
+    public Person(String firstname, String lastname, String address, String city, String zip, String phone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
